2dc3e3552e0d17d4f62cd981732acd7e7f95d388
Majaz <majazulhaque123@gmail.com>
2023-07-05 08:32:57 +0530

    creating Sidebar component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Sidebar from './component/Sidebar';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <Sidebar/>
     </div>
   );
 }
---
+++ src/component/Sidebar.js
@@ -0,0 +1,37 @@
+import React from "react";
+import "./Sidebar.css";
+function Sidebar() {
+  return (
+    <div id="sidebar">
+      <div id="user_detail">
+        <span id="img">RR</span>
+        <span id="green"></span>
+        <div id="username">
+          <span id="name">Rolande Raimondi</span>
+          <span>Research Nurse</span>
+        </div>
+      </div>
+      <div id="conversation">
+        <div id="conversation_name">
+          <span>Conversations</span>
+          <i class="fa-solid fa-plus"></i>
+        </div>
+        <div className="conversation_item">
+          <span className="item_img">#</span>
+          <span className="item_name">Poland Office</span>
+        </div>
+        <div className="conversation_item" id="introduction">
+          <span className="item_img" id="intro">
+            #
+          </span>
+          <span className="item_name">Introduction</span>
+        </div>
+        <div className="conversation_item">
+          <span className="item_img">#</span>
+          <span className="item_name">India Office</span>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default Sidebar;
