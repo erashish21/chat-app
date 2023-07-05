054527746b8dd6be1c6eccef5f4d682b84fc86ff
Majaz <majazulhaque123@gmail.com>
2023-07-05 10:18:47 +0530

    Add emoji in input container

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,12 +1,18 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import "./Home.css";
 
-const user_list = ["Alan", "Bob", "Carol", "Dean", "Elin"]; 
+import Picker from "emoji-picker-react";
+
+
+const user_list = ["Alan", "Bob", "Carol", "Dean", "Elin"];
 // const color_list = ["green", "yellow", "red", "purple", "orange"];
 
 function Home() {
-    const [messages, setMessages] = useState([]);
+  const [messages, setMessages] = useState([]);
   const [messageInput, setMessageInput] = useState("");
+  const [showEmojiPicker, setShowEmojiPicker] = useState(false);
+const [selectedEmoji, setSelectedEmoji] = useState(null);
+const emojiPickerRef = useRef(null);
 
   const handleInputChange = (event) => {
     setMessageInput(event.target.value);
@@ -14,15 +20,16 @@ function Home() {
 
   const handleSendMessage = () => {
     if (messageInput.trim() !== "") {
-      const randomUser = user_list[Math.floor(Math.random() * user_list.length)];
-      
+      const randomUser =
+        user_list[Math.floor(Math.random() * user_list.length)];
+
       const hours = new Date().getHours();
       const minutes = new Date().getMinutes();
       const newMessage = {
         username: randomUser,
         message: messageInput,
-        hours:hours,
-        minutes:minutes,
+        hours: hours,
+        minutes: minutes,
         likes: 0,
       };
       setMessages([...messages, newMessage]);
@@ -36,31 +43,47 @@ function Home() {
     setMessages(updatedMessages);
   };
 
-  const handleColor = (val) =>{
+  const handleShowEmojiPicker = () => {
+    if(showEmojiPicker == true){
+        setShowEmojiPicker(false);
+    }
+    else{
+        setShowEmojiPicker(true);
+    }
+    
+  };
+  
+
+  const handleEmojiSelect = (emoji) => {
+    setSelectedEmoji(emoji);
+    setMessageInput(messageInput + emoji.emoji);
+  };
+  
+
+  const handleColor = (val) => {
     let ans = "";
     switch (val) {
-        case 'A':
-            ans = 'green';
-            break;
-        case 'B':
-            ans = "yellow";
-            break;
-        case 'C':
-            ans = "red";
-            break;
-        case 'D':
-            ans = "purple";
-            break;
-        case 'E':
-            ans = "orange";
-            break;
-        default:
-            ans = 'red';
-            break;
+      case "A":
+        ans = "green";
+        break;
+      case "B":
+        ans = "yellow";
+        break;
+      case "C":
+        ans = "red";
+        break;
+      case "D":
+        ans = "purple";
+        break;
+      case "E":
+        ans = "orange";
+        break;
+      default:
+        ans = "red";
+        break;
     }
     return ans;
-  }
-
+  };
 
   return (
     <div className="home">
@@ -78,34 +101,58 @@ function Home() {
       </div>
       <div className="chat-container">
         <div className="message-thread">
-        {messages.map((message, index) => (
-          <div key={index} className="message">
-            <div className="message_details">
-              <div className="message-user">
-                <span
-                  className= {`img ${handleColor(message.username.charAt(0))}`}
-                >
-                  {message.username.charAt(0)}
-                </span>
-                <span className="username">{message.username}</span>
-                <span className="time">{message.hours}:{message.minutes}</span>
-              </div>
-              <div className="message-content">
-                <span className="message-text">{message.message}</span>
-                <i onClick={()=> handleLike(index)} className="fa-solid fa-thumbs-up"></i>{" "} &nbsp;{message.likes}
+          {messages.map((message, index) => (
+            <div key={index} className="message">
+              <div className="message_details">
+                <div className="message-user">
+                  <span
+                    className={`img ${handleColor(message.username.charAt(0))}`}
+                  >
+                    {message.username.charAt(0)}
+                  </span>
+                  <span className="username">{message.username}</span>
+                  <span className="time">
+                    {message.hours}:{message.minutes}
+                  </span>
+                </div>
+                <div className="message-content">
+                  <span className="message-text">{message.message}</span>
+                  <i
+                    onClick={() => handleLike(index)}
+                    className="fa-solid fa-thumbs-up"
+                  ></i>{" "}
+                  &nbsp;{message.likes}
+                </div>
               </div>
             </div>
-          </div>
-        ))}
+          ))}
         </div>
+        {showEmojiPicker && (
+      <div ref={emojiPickerRef} className="emoji-picker-container">
+        <Picker
+          onEmojiClick={handleEmojiSelect}
+          disableSearchBar
+          native
+          emojiTooltip
+          pickerStyle={{ width: 'auto' }}
+        />
+      </div>
+    )}
+
         <div className="input-container">
           <input
-          value={messageInput}
-          onChange={handleInputChange}
-            type="text"  
+            value={messageInput}
+            onChange={handleInputChange}
+            type="text"
             placeholder="Type your message..."
           />
-            <i onClick={handleSendMessage} id="send" class="fa-solid fa-paper-plane"></i>
+          <i style={{marginTop:"0.6rem",marginLeft:"-2rem",marginRight:'3rem',fontSize:'1.2rem'}} onClick={handleShowEmojiPicker} className="fa-solid fa-grin"></i>
+          <i
+          style={{marginRight:"0.8rem",marginLeft:"-1rem",fontSize:'1.4rem'}}
+            onClick={handleSendMessage}
+            id="send"
+            class="fa-solid fa-paper-plane"
+          ></i>
         </div>
       </div>
     </div>
